18922028b21e3dcfa7d14c29d43a4d72970953fb
vaughn <vaughnanton@gmail.com>
2021-01-21 16:08:29 -0800

    handling user input with forms and events

---
+++ react/pics/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+            {/* onSubmit below can be called anything */}
+            <SearchBar onSubmit={this.onSearchSubmit} />
+        </div>
+        );
+    }
+}
+
+export default App;
