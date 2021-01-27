0d422c8651d68ea0a946f3a9c31c696058ae9359
vaughn <vaughnanton@gmail.com>
2021-01-26 23:22:38 -0800

    build list output

--- react/pics/src/api/unsplash.js
+++ react/pics/src/api/unsplash.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
+        Authorization: 'Client-ID K-SKeoobwW5XYe3r31i1nxSmXASWJzugcS4WE-J9JeA'
     }
 });
--- react/pics/src/components/App.js
+++ react/pics/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash'; 
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
     state = { images: [] };
@@ -24,7 +25,7 @@ class App extends React.Component {
             <div className="ui container" style={{marginTop: '10px'}}>
             {/* onSubmit below can be called anything */}
             <SearchBar onSubmit={this.onSearchSubmit} />
-            Found: {this.state.images.length} images
+            <ImageList images={this.state.images} />
         </div>
         );
     }
