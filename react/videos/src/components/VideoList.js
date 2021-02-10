859080e908d62cc8c34dd85a20d57407c162c273
vaughn <vaughnanton@gmail.com>
2021-02-09 19:08:42 -0800

    complete video search app

--- react/videos/src/components/VideoList.js
+++ react/videos/src/components/VideoList.js
@@ -1,10 +1,17 @@
-import react from 'react';
+import React from 'react';
 import VideoItem from './VideoItem';
 
 // destructured videos out from props
-const VideoList = ({ videos }) => {
+// so insted of props.video you can just do video 
+const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video} />;
+        return (
+            <VideoItem 
+                key={video.id.videoId} 
+                onVideoSelect={onVideoSelect} 
+                video={video} 
+            />
+        );
     });
 
     return <div className="ui relaxed divided list">{renderedList}</div>;
