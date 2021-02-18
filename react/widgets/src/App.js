0c6f9a765ca353c4509c0a13824495f0d672caa2
vaughnanton <vaughnanton@gmail.com>
2021-02-18 15:36:38 -0800

    initialize dropdown build

--- react/widgets/src/App.js
+++ react/widgets/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -16,11 +17,28 @@ const items = [
         content: 'You use React by writing components'
     }
 ]
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+]
+
 export default () => {
     return (
         <div>
             {/* <Accordion items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown options={options} />
         </div>
     )
 };
