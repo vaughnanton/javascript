1b467c3d4730306ad9cb4cdfb480030ed2e74354
vaughnanton <vaughnanton@gmail.com>
2021-03-16 16:04:55 -0700

    complete blog app with redux thunk

--- react/blog/src/components/PostList.js
+++ react/blog/src/components/PostList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
 
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     renderList() {
@@ -18,6 +19,7 @@ class PostList extends React.Component {
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId} />
                     </div>
                 </div>
             );
@@ -36,5 +38,5 @@ const mapStateToProps = (state) => {
 
 export default connect(
     mapStateToProps, 
-    { fetchPosts: fetchPosts }
+    { fetchPostsAndUsers: fetchPostsAndUsers }
 )(PostList);
