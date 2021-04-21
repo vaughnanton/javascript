b0c03bb8484e007b0e72d7272c8d5da13e4d066f
Vaughn Antonyan <26296924+vaughnanton@users.noreply.github.com>
2021-04-21 12:44:49 -0700

    redux form setup

--- react/streams/client/src/reducers/index.js
+++ react/streams/client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+// change named import to formReducer to be more descriptive
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
