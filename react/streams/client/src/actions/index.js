5880ce5ba4807e949642dc9153db84a07e330df3
Vaughn Antonyan <26296924+vaughnanton@users.noreply.github.com>
2021-05-03 16:18:31 -0700

    create our own history object from browuser-router to navigate user after post

--- react/streams/client/src/actions/index.js
+++ react/streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -28,6 +29,9 @@ export const createStream = (formValues) => {
         const response = await streams.post('/streams', { ...formValues, userId });
 
         dispatch({ type: CREATE_STREAM, payload: response.data });
+
+        // programmatic navigation to get user back to root route
+        history.push('/');
     }
 };
 
