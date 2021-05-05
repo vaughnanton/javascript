d857d7224ac862acc16e6d73d309dbc67ee2c325
Vaughn Antonyan <26296924+vaughnanton@users.noreply.github.com>
2021-05-05 16:14:56 -0700

    complete delete stream functionality

--- react/streams/client/src/actions/index.js
+++ react/streams/client/src/actions/index.js
@@ -59,4 +59,6 @@ export const deleteStream = (id) => async dispatch => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
+
+    history.push('/');
 }
